0b215015d4a9a20aba78d7ff30bf9b541df78b88
Ibrahim <ibrahimelothmani98@gmail.com>
2025-02-12 20:45:02 +0100

    Sequential & Binary Search

---
+++ Binary Search/src/Main.java
@@ -0,0 +1,25 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(BinarySearch(arr, 9, 5));
+    }
+
+    //	Binary	Search	Algorithm	–	Iterative	Way
+    public static int BinarySearch(int arr[], int size, int value) {
+        int mid;
+        int low = 0;
+        int high = size - 1;
+        while (low <= high) {
+            mid = low + (high - low) / 2; // To	avoid	the	overflow
+            if (arr[mid] == value)
+                return mid;
+            else if (arr[mid] < value)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return -1;
+    }
+}
