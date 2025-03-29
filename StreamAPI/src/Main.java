a84454cff1b2a47274bb57919d6f9ca5a7f1cedb
Ibrahim <ibrahimelothmani98@gmail.com>
2025-03-29 01:35:30 +0100

    use the stream to see the result in an array

--- StreamAPI/src/Main.java
+++ StreamAPI/src/Main.java
@@ -5,11 +5,54 @@ public class Main {
 
         int[] numbers = {0, 6, 4, 5, 7, 8, 9, 3, 2, 1, 10};
 
-        for (int i = 0; i < numbers.length; i++) {
-
-            System.out.println(
-                    Arrays.stream(numbers)
-                            .map(n -> n % 2));
-        }
+//        for (int number : numbers) {
+//            System.out.print(number + "\n");
+//            System.out.println(".............................");
+            System.out.println(Arrays.stream(
+                    numbers).max().getAsInt()
+            );
+            System.out.println(Arrays.stream(
+                    numbers).min().getAsInt()
+            );
+            System.out.println(Arrays.stream(
+                    numbers).sum()
+            );
+            System.out.println(Arrays.stream(
+                    numbers).average().getAsDouble()
+            );
+            System.out.println(Arrays.stream(
+                    numbers).count()
+            );
+            System.out.println(Arrays.stream(
+                    numbers).filter(number -> number % 2 == 0).count()
+            );
+            System.out.println(Arrays.stream(
+                    numbers).filter(number -> number % 2 != 0).count()
+            );
+            System.out.println(Arrays.stream(
+                    numbers).filter(number -> number % 2 == 0).sum()
+            );
+            System.out.println(Arrays.stream(
+                    numbers).filter(number -> number % 2 != 0).sum()
+            );
+            System.out.println(Arrays.stream(
+                    numbers).filter(number -> number % 2 == 0).average().getAsDouble()
+            );
+            System.out.println(Arrays.stream(
+                    numbers).filter(number -> number % 2 != 0).average().getAsDouble()
+            );
+            System.out.println(Arrays.stream(
+                    numbers).filter(number -> number % 2 == 0).max().getAsInt()
+            );
+            System.out.println(Arrays.stream(
+                    numbers).filter(number -> number % 2 != 0).max().getAsInt()
+            );
+            System.out.println(Arrays.stream(
+                    numbers).filter(number -> number % 2 == 0).min().getAsInt()
+            );
+            System.out.println(Arrays.stream(
+                    numbers).filter(number -> number % 2 != 0).min().getAsInt()
+            );
+//        }
     }
 }
